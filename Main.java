67d4eb67fa8ed8393f0ea50c597c08d4a72e512d
Leonardo Tamayo <102887316+Lets21@users.noreply.github.com>
2025-06-25 00:42:58 +0000

    feat: Añadir saludo multilingüe (es/en/fr)

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("***************");
+        System.out.println("* JAVA PROJECT *");
+        System.out.println("***************");
+
+        Saludo saludo = new Saludo();
+        System.out.println(saludo.generarSaludo("Mundo", "en"));
+        System.out.println(saludo.generarSaludo("Mundo", "es"));
+        System.out.println(saludo.generarSaludo("Mundo", "fr"));
+
+        Despedida despedida = new Despedida();
+        System.out.println(despedida.generarDespedida("Mundo"));
+    }
+}
